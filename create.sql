0dc9e2ccd5072bfb9afdf602a37a52f12f2c4979
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-08-12 14:52:49 -0400

    improved

--- create.sql
+++ create.sql
@@ -10,3 +10,52 @@ create table categories(id INTEGER PRIMARY KEY AUTOINCREMENT,
                         name varchar,
                         subject varchar
                         );
+
+INSERT INTO categories (
+name,
+subject)
+VALUES
+(
+"Ruby",
+"programming");
+
+INSERT INTO categories (
+name,
+subject)
+VALUES
+(
+"Comedy",
+"Stand up comedy"
+);
+
+INSERT INTO terms (
+ name,
+ definition,
+ author,
+ web_link,
+ category_id)
+VALUES
+ (
+ "active record",
+ "an ORM for ruby",
+ "David Heinemeier Hansson",
+ "http://goo.gl/dbXsUX",
+ 1),
+ (
+ "modules",
+ "a way of grouping together methods, classes, and constants",
+ "Andy Hunt, Dave Thomas",
+ "http://goo.gl/cee6oc",
+ 1),
+ (
+ "Ruby Tk",
+ "a graphical user interface (GUI) for Ruby",
+ "David Heinemeier Hansson",
+ "http://goo.gl/JD1zMe",
+ 1),
+ (
+ "riffing",
+ "verbally bantering with the audience; also known as crowd work.",
+ "Greg Dean",
+ "http://goo.gl/Md0cr1",
+ 2);
--- iron_slag.rb
+++ iron_slag.rb
@@ -1,18 +1,18 @@
 require 'sqlite3'
 require 'active_record'
-require 'sinatra'
-require 'rack-flash'
-
-require 'sinatra/reloader' if development?
+# require 'sinatra'
+# require 'rack-flash'
+#
+# require 'sinatra/reloader' if development?
+#
+# use Rack::MethodOverride
+# use Rack::Flash
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "iron_slag.db"
 )
-
-use Rack::MethodOverride
-use Rack::Flash
 
 class Category < ActiveRecord::Base
   has_many :terms
@@ -21,3 +21,14 @@ end
 class Term < ActiveRecord::Base
   belongs_to :category
 end
+
+terms = Term.all
+terms.each do |term|
+  # also print out the subject
+  puts "There is a term called #{term.name} in the category #{term.category.name} which is within the #{term.category.subject} category"
+end
+
+# Iterate over all the categories
+# For each category print the name of the name of the category
+# Then for THAT category, iterate over all the terms
+# For that term print it's name
